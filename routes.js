e05b0d73c2047c3c4345e41110c7638eb9ff26f6
oksanabaza <oksana.misiura.cld@gmail.com>
2023-07-05 15:23:02 +0100

    track controller fixed

--- routes.js
+++ routes.js
@@ -1,27 +1,28 @@
 import express from "express";
-import { dashboardController } from "./controllers/dashboard-controller.js";
-import { aboutController } from "./controllers/about-controller.js";
-import { playlistController } from "./controllers/playlist-controller.js";
 import { accountsController } from "./controllers/accounts-controller.js";
+import { dashboardController } from "./controllers/dashboard-controller.js";
+import { playlistController } from "./controllers/playlist-controller.js";
 import { trackController } from "./controllers/track-controller.js";
+import { aboutController } from "./controllers/about-controller.js";
 
 export const router = express.Router();
 
-//login
 router.get("/", accountsController.index);
+
 router.get("/login", accountsController.login);
 router.get("/signup", accountsController.signup);
 router.get("/logout", accountsController.logout);
 router.post("/register", accountsController.register);
 router.post("/authenticate", accountsController.authenticate);
 
-// router.get("/", dashboardController.index);
 router.get("/dashboard", dashboardController.index);
-router.get("/about", aboutController.index);
 router.post("/dashboard/addplaylist", dashboardController.addPlaylist);
+router.get("/dashboard/deleteplaylist/:id", dashboardController.deletePlaylist);
 router.get("/playlist/:id", playlistController.index);
 router.post("/playlist/:id/addtrack", playlistController.addTrack);
-router.get("/dashboard/deleteplaylist/:id", dashboardController.deletePlaylist);
 router.get("/playlist/:playlistid/deletetrack/:trackid", playlistController.deleteTrack);
 router.get("/playlist/:playlistid/edittrack/:trackid", trackController.index);
 router.post("/playlist/:playlistid/updatetrack/:trackid", trackController.update);
+
+router.get("/about", aboutController.index);
+router.get("/logout", accountsController.logout);
