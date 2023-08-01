675edbd6df9e59b9d847247bf56f58341291e237
oksanabaza <oksana.misiura.cld@gmail.com>
2023-08-01 18:18:36 +0100

    init

--- routes.js
+++ routes.js
@@ -7,4 +7,4 @@ export const router = express.Router();
 router.get("/", dashboardController.index);
 router.get("/dashboard", dashboardController.index);
 router.get("/about", aboutController.index);
-router.post("/dashboard/addplaylist", dashboardController.addPlaylist);
+router.post("/dashboard/addstation", dashboardController.addStation);
